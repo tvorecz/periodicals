01f591b5bd70917cb0e3f72f6dc2fc5f7374f8e5
tvorecz <tvorecz@gmail.com>
2018-12-17 23:40:57 +0300

    DataBase schema

--- backups/Periodicals.db.sql
+++ backups/Periodicals.db.sql
@@ -2,8 +2,12 @@ BEGIN TRANSACTION;
 CREATE TABLE IF NOT EXISTS `Users` (
 	`idUser`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
 	`login`	TEXT NOT NULL UNIQUE,
-	`codifiedPassword`	TEXT NOT NULL,
-	`idRole`	INTEGER
+	`password`	TEXT NOT NULL,
+	`idRole`	INTEGER NOT NULL,
+	`email`	TEXT NOT NULL UNIQUE,
+	`idDefaultLocal`	INTEGER NOT NULL DEFAULT 1,
+	FOREIGN KEY(`idRole`) REFERENCES `UserRoles`(`idRole`),
+	FOREIGN KEY(`idDefaultLocal`) REFERENCES `Internationalization`(`idLocal`)
 );
 CREATE TABLE IF NOT EXISTS `UserSubscriptions` (
 	`idUserSubscription`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
@@ -11,12 +15,17 @@ CREATE TABLE IF NOT EXISTS `UserSubscriptions` (
 	`idSubscriptionVariant`	INTEGER NOT NULL,
 	`dateBegin`	TEXT NOT NULL,
 	`dateEnd`	TEXT NOT NULL,
-	`idPayment`	INTEGER NOT NULL
+	`idPayment`	INTEGER NOT NULL,
+	FOREIGN KEY(`idPayment`) REFERENCES `Payments`(`idPayment`),
+	FOREIGN KEY(`idAddress`) REFERENCES `UserAddresses`(`idAddress`),
+	FOREIGN KEY(`idSubscriptionVariant`) REFERENCES `SubscriptionVariants`(`idSubscriptionVariant`)
 );
 CREATE TABLE IF NOT EXISTS `UserRoles` (
 	`idRole`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
 	`nameRole`	TEXT NOT NULL
 );
+INSERT INTO `UserRoles` (idRole,nameRole) VALUES (1,'администратор'),
+ (2,'подписчик');
 CREATE TABLE IF NOT EXISTS `UserAddresses` (
 	`idAddress`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
 	`address`	TEXT NOT NULL,
@@ -36,6 +45,15 @@ CREATE TABLE IF NOT EXISTS `SubscriptionTypes` (
 	`idSubscriptionType`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
 	`nameSubscriptionType`	TEXT NOT NULL
 );
+INSERT INTO `SubscriptionTypes` (idSubscriptionType,nameSubscriptionType) VALUES (1,'месячная'),
+ (2,'полугодовая'),
+ (3,'квартальная'),
+ (4,'ведомственная месячная'),
+ (5,'ведомственная полугодовая'),
+ (6,'ведомственная квартальная'),
+ (7,'льготная месячная'),
+ (8,'льготная полугодовая'),
+ (9,'льготная квартальная');
 CREATE TABLE IF NOT EXISTS `Periodicals` (
 	`idPeriodical`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
 	`namePeriodical`	TEXT NOT NULL,
@@ -47,4 +65,10 @@ CREATE TABLE IF NOT EXISTS `Payments` (
 	`amount`	REAL NOT NULL,
 	`payStatus`	INTEGER NOT NULL DEFAULT 0
 );
+CREATE TABLE IF NOT EXISTS `Internationalization` (
+	`idLocal`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+	`nameLocal`	TEXT NOT NULL UNIQUE
+);
+INSERT INTO `Internationalization` (idLocal,nameLocal) VALUES (1,'RU'),
+ (2,'EN');
 COMMIT;
